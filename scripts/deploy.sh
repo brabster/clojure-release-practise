d45af9c2c922c2b9546fb01b66cc08d8b527fa39
Paul Brabban <paul.brabban@gmail.com>
2016-10-31 07:15:10 +0000

    escaping

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export PROJECT_VERSION=${TRAVIS_TAG:-"${TRAVIS_BRANCH//[/]-/}-SNAPSHOT"}
+export PROJECT_VERSION=${TRAVIS_TAG:-"${TRAVIS_BRANCH//[\/]-/}-SNAPSHOT"}
 echo "Project version is \"$PROJECT_VERSION\""
 
 lein deploy
