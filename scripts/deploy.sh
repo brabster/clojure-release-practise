86c81a3b467d8b18720c45ec6887495f971e3a44
Paul Brabban <paul.brabban@gmail.com>
2016-10-31 07:41:58 +0000

    and deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,4 +7,4 @@ SNAPSHOT_VERSION="${TRAVIS_BRANCH//[\/]/-}-SNAPSHOT"
 export PROJECT_VERSION=${STABLE_VERSION:-"${SNAPSHOT_VERSION}"}
 echo "Project version is \"$PROJECT_VERSION\""
 
-#lein deploy
+lein deploy
